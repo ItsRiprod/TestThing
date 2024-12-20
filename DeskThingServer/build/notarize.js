fa7d1b1f1e6d34774b9d372ce77d3dc7b6482fda
ItsRiprod <riprodgames@gmail.com>
2024-12-19 23:21:49 -0500

    Removed stuff to follow strict tutorial

--- DeskThingServer/build/notarize.js
+++ DeskThingServer/build/notarize.js
@@ -1,59 +1,20 @@
-const { notarize } = require('@electron/notarize')
-const path = require('path')
+require('dotenv').config()
+const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing(context) {
-  if (
-    context.electronPlatformName !== 'darwin' ||
-    process.env.CSC_IDENTITY_AUTO_DISCOVERY === 'false'
-  ) {
-    console.log('Skipping notarization')
-    return
-  }
-  console.debug('Notarizing...')
-  console.debug('Environment variables:')
-  console.debug('APPLE_TEAM_ID:', process.env.APPLE_TEAM_ID)
-  console.debug('APPLE_ID:', process.env.APPLE_ID)
-
-  const appPath = path.normalize(path.join(context.appOutDir, `DeskThing.app`))
-  const appleId = process.env.APPLE_ID
-  const teamId = process.env.APPLE_TEAM_ID
-  const appleIdPassword = process.env.APPLE_APP_SPECIFIC_PASSWORD
-  if (!appleId) {
-    console.warn('Not notarizing: Missing APPLE_ID environment variable')
-    return
-  }
-  if (!appleIdPassword) {
-    console.warn('Not notarizing: Missing APPLE_APP_SPECIFIC_PASSWORD environment variable')
-    return
-  }
-  if (!teamId) {
-    console.warn('Not notarizing: Missing APPLE_TEAM_ID environment variable')
+  const { electronPlatformName, appOutDir } = context
+  if (electronPlatformName !== 'darwin') {
     return
   }
 
-  console.log('::group::Notarization Details')
-  console.log('Team ID:', teamId)
-  console.log('Apple ID:', appleId)
-  console.log('::endgroup::')
+  const appName = context.packager.appInfo.productFilename
 
-  try {
-    console.debug('Notarization options:')
-    console.debug('appPath:', appPath)
-    console.debug('teamId:', teamId)
-    console.debug('appleId:', appleId)
-
-    const notarizeOptions = {
-      tool: 'notarytool',
-      appPath,
-      appleId,
-      appleIdPassword,
-      teamId
-    }
-
-    return await notarize(notarizeOptions)
-  } catch (error) {
-    console.error('Error when notarizing app: ', error, '\nSkipping...')
-  }
-
-  return
+  return await notarize({
+    tool: 'notarytool',
+    teamId: process.env.APPLE_TEAM_ID,
+    appBundleId: 'com.deskthing.app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD
+  })
 }
